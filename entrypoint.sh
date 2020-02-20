03fdfe56d8958931135608b56597f449e0e060ec
Viaceslav-Kodesh <54391135+Viaceslav-Kodesh@users.noreply.github.com>
2020-02-20 19:14:44 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,3 +3,10 @@
 echo "Hello $1"
 time=$(date)
 echo ::set-output name=time::$time
+sudo apt update
+sudo apt install python-pip
+sudo pip install jsonlib
+sudo pip install python-novaclient
+sudo pip install credentials
+sudo pip install python-openstacksdk
+sudo apt update
