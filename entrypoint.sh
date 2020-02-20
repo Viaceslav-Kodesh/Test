362355cae4099436f5c82deaa6152bc99fb0ca6b
Viaceslav-Kodesh <54391135+Viaceslav-Kodesh@users.noreply.github.com>
2020-02-20 19:32:39 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,9 +10,3 @@ pip install python-novaclient
 pip install credentials
 pip install python-openstacksdk
 apt update
-OS_AUTH_URL: ${{secrets.OS_AUTH_URL}}
-OS_PASSWORD: ${{secrets.OS_PASSWORD}}
-OS_TENANT_NAME: ${{secrets.OS_TENANT_NAME}}
-OS_USERNAME: ${{secrets.OS_USERNAME}}
-OS_USER_DOMAIN_NAME: ${{secrets.OS_USER_DOMAIN_NAME}}
-python demo.py
